7c7a6f891829285713df38098ce5e45a7d8bb467
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2024-03-17 15:38:35 +0100

    Example from https://json-schema.org/learn/miscellaneous-examples

--- test/test.lua
+++ test/test.lua
@@ -1,13 +1,19 @@
+-- ugshell -ex ../../plugins/JSONToolkit/test/test.lua --path $UG$_ROOT/plugins/JSONToolkit/test/basic-01/
 
 ug_load_script("ug_util.lua")
 local path = util.GetParam("--path", "")
 
+print("Create validator...")
+v0=JSONSchemaValidator();
+
+print("Init schema...")
 local jschema=JSON(); 
 JSON_load_from_file(jschema, path.."schema.json");
+print(JSON_dump(jschema))
+v0:init(jschema)
 
+print("Validate document...")
 local jdocument=JSON(); 
 JSON_load_from_file(jdocument, path.."instance.json"); 
-
-v0=JSONSchemaValidator();
-v0:init(jschema)
+print(JSON_dump(jdocument))
 v0:validate_document(jdocument)
