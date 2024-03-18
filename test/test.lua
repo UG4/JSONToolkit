260275873f8797d67737ab11148c75a15dd317f6
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2024-03-18 07:56:59 +0100

    Update for tests.

--- test/test.lua
+++ test/test.lua
@@ -1,4 +1,4 @@
--- ugshell -ex ../../plugins/JSONToolkit/test/test.lua --path $UG$_ROOT/plugins/JSONToolkit/test/basic-01/
+-- ugshell -ex ../../plugins/JSONToolkit/test/test.lua --path $UG$_ROOT/plugins/JSONToolkit/test/basic-01-person/
 
 ug_load_script("ug_util.lua")
 local path = util.GetParam("--path", "")
@@ -15,5 +15,5 @@ v0:init(jschema)
 print("Validate document...")
 local jdocument=JSON(); 
 JSON_load_from_file(jdocument, path.."instance.json"); 
-print(JSON_dump(jdocument))
+print("|"..JSON_dump(jdocument).."|")
 v0:validate_document(jdocument)
